885ae98eb7b024f1876cc658415d8b3190f307de
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-09-08 04:41:22 -0600

    Added files and also updated my link in html

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ let todayTime = document.querySelector(".current__time");
 let today = document.querySelector(".current__day");
 const localDate = new Date();
 const options = {weekday: 'long'}
-
 const day = localDate.toLocaleDateString('en-US', options)
-
 today.textContent = day
-todayTime.textContent = utcString
+
+
+function updateTime(){
+    const utcString = localDate.getTime()
+    todayTime.textContent = utcString
+}
+setInterval(updateTime, 1000)
